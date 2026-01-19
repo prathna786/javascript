5eb4b12da710ad4e04ec9b964216e1f28ba7a1f3
prathna786 <prathnaviradiya@gmail.com>
2026-01-19 20:36:57 +0530

    update

---
+++ level1/first.js
@@ -0,0 +1,24 @@
+let name = "Tony stark";//string 
+let age = 29;//number
+let totalPrice = 3000;
+isFollow = true;//boolean
+const PI = 3.14;//not change value
+let x;//undifined
+let a = null;//object
+console.log(age);
+{
+    let a = 15;
+    console.log(a);
+}
+//const key can update value only 
+const student = {
+    name: "Rahul kumar",
+    age: 30,
+    cgpa: 8.2,
+    isPass: true,
+};
+student["age"]=student["age"]+1;//increase object variable value
+student["name"]="Rahul Sharma";//modify name in object variable
+console.log(student["name"]);
+// primitive data types (7)=number,string,boolean,undefined 
+// non-primitive data Types()
