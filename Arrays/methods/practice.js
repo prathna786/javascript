0806b1129317ffbae1f5cdbe11a603be53790f3e
prathna786 <prathnaviradiya@gmail.com>
2026-01-28 18:41:41 +0530

    update

---
+++ Arrays/methods/practice.js
@@ -0,0 +1,12 @@
+let companies = ["Blooomberg","Microsoft","Uber","Google","IBM","Netflix"];
+
+console.log(companies);
+
+companies.shift();
+console.log(companies);
+
+companies.splice(1,1,"Ola");
+console.log(companies);
+
+companies.push("Amazon");
+console.log(companies);
